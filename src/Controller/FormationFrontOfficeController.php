c1da6595f3040e0e35ce3530869a5a608852004b
Haythem Dridi <haithemdridiweb@gmail.com>
2025-04-24 21:31:58 +0100

    afficher details formation

---
+++ src/Controller/FormationFrontOfficeController.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Controller;
+
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FormationFrontOfficeController extends AbstractController
+{
+    #[Route('/frontoffice/formations', name: 'app_user_formation')]
+    public function index(FormationRepository $formationRepository): Response
+    {
+        return $this->render('formations/liste_formation.html.twig', [
+            'formations' => $formationRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/frontoffice/formations/{id}', name: 'app_user_formation_details')]
+    public function details(FormationRepository $formationRepository, $id): Response
+    {
+
+        $formation = $formationRepository->find($id);
+
+        if (! $formation) {
+            throw $this->createNotFoundException('Formation not found');
+        }
+
+        return $this->render('formations/details_formation.twig', [
+            'formation' => $formation,
+        ]);
+    }
+
+}
